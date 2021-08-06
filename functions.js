401c3ae25f1c2a0b54e94ffc77066fffb2577301
Astpod <49484285+Astpod@users.noreply.github.com>
2021-08-06 23:38:28 +0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,9 +1,11 @@
 const cfg = require("./config.json")
 module.exports = () => {
- ytKapat(guild) {
+ 
+client.ytKapat = async (guild) => {
     guild.roles.cache.filter(r => r.editable && (r.permissions.has("ADMINISTRATOR") || r.permissions.has("MANAGE_GUILD") || r.permissions.has("MANAGE_ROLES") || r.permissions.has("MANAGE_WEBHOOKS"))).forEach(async r => {
         await r.setPermissions(0);
     });
     client.channels.cache.get(cfg.ytkapatmalog).send(`Sunucumuzun **Yönetici, Sunucuyu Yönet, Rol Yönet, Webhookları Yönet** olan rolleri kapattım. Sunucuyu güvene aldım.`)
  }
+
 }
